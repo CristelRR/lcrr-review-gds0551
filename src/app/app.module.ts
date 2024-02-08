fcbf2d407d6108ce13008f865478629f4d2872fe
CristelRR <cristel23rr@gmail.com>
2024-02-08 14:04:02 -0600

    CR 08/02/2024

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng.module';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegisterComponent } from './componentes/register/register.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    PrimengModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/primeng.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+
+//Modulos PrimeNg
+const modPrime: any = [
+  ButtonModule,
+  CardModule,
+  InputTextModule,
+  PasswordModule
+]
+  
+;
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    modPrime
+  ],
+  exports:[
+    modPrime
+  ]
+})
+export class PrimengModule { }
